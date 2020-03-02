57d05c604e0a04db567df5eb77209101a44aded4
rdavila <rdavila@consumeraffairs.con>
2020-03-01 21:49:28 -0600

    delete old project, add new folder for step by step project

---
+++ POM_Model/Pages/Page_methods.py
@@ -0,0 +1,11 @@
+
+class Samples:
+    """
+    Locators:
+    """
+    # Sample url files #
+    sample_urls = "/Users/rdavila/Workspace/playbox/POM_Model/Pages/url_sample.txt"
+
+    def parse_url(url):
+        parsed_url = str(url.strip())
+        return parsed_url
